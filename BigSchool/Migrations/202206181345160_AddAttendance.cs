69f7ceb9fc4524839b2d4341732ddbf15ad53cb6
Admin <Admin@ADMIN>
2022-06-19 14:53:42 +0700

    62

---
+++ BigSchool/Migrations/202206181345160_AddAttendance.cs
@@ -0,0 +1,40 @@
+﻿namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddAttendance : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Attendances", "Attendee_Id", "dbo.AspNetUsers");
+            DropIndex("dbo.Attendances", new[] { "CourseID" });
+            DropIndex("dbo.Attendances", new[] { "Attendee_Id" });
+            DropColumn("dbo.Attendances", "AttendeeID");
+            RenameColumn(table: "dbo.Attendances", name: "Attendee_Id", newName: "AttendeeId");
+            DropPrimaryKey("dbo.Attendances");
+            AlterColumn("dbo.Attendances", "AttendeeId", c => c.String(nullable: false, maxLength: 128));
+            AlterColumn("dbo.Attendances", "AttendeeId", c => c.String(nullable: false, maxLength: 128));
+            AddPrimaryKey("dbo.Attendances", new[] { "CourseId", "AttendeeId" });
+            CreateIndex("dbo.Attendances", "CourseId");
+            CreateIndex("dbo.Attendances", "AttendeeId");
+            AddForeignKey("dbo.Attendances", "AttendeeId", "dbo.AspNetUsers", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Attendances", "AttendeeId", "dbo.AspNetUsers");
+            DropIndex("dbo.Attendances", new[] { "AttendeeId" });
+            DropIndex("dbo.Attendances", new[] { "CourseId" });
+            DropPrimaryKey("dbo.Attendances");
+            AlterColumn("dbo.Attendances", "AttendeeId", c => c.String(maxLength: 128));
+            AlterColumn("dbo.Attendances", "AttendeeId", c => c.Int(nullable: false));
+            AddPrimaryKey("dbo.Attendances", new[] { "CourseID", "AttendeeID" });
+            RenameColumn(table: "dbo.Attendances", name: "AttendeeId", newName: "Attendee_Id");
+            AddColumn("dbo.Attendances", "AttendeeID", c => c.Int(nullable: false));
+            CreateIndex("dbo.Attendances", "Attendee_Id");
+            CreateIndex("dbo.Attendances", "CourseID");
+            AddForeignKey("dbo.Attendances", "Attendee_Id", "dbo.AspNetUsers", "Id");
+        }
+    }
+}
---
+++ BigSchool/Migrations/202206181413326_AddAtendance.cs
@@ -0,0 +1,34 @@
+﻿namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddAtendance : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Attendances", "Attendee_Id", "dbo.AspNetUsers");
+            DropIndex("dbo.Attendances", new[] { "CourseID" });
+            DropIndex("dbo.Attendances", new[] { "Attendee_Id" });
+            DropPrimaryKey("dbo.Attendances");
+            AddColumn("dbo.Attendances", "AttendanceId", c => c.String(nullable: false, maxLength: 128));
+            AddPrimaryKey("dbo.Attendances", new[] { "CourseId", "AttendanceId" });
+            CreateIndex("dbo.Attendances", "CourseId");
+            DropColumn("dbo.Attendances", "AttendeeID");
+            DropColumn("dbo.Attendances", "Attendee_Id");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Attendances", "Attendee_Id", c => c.String(maxLength: 128));
+            AddColumn("dbo.Attendances", "AttendeeID", c => c.Int(nullable: false));
+            DropIndex("dbo.Attendances", new[] { "CourseId" });
+            DropPrimaryKey("dbo.Attendances");
+            DropColumn("dbo.Attendances", "AttendanceId");
+            AddPrimaryKey("dbo.Attendances", new[] { "CourseID", "AttendeeID" });
+            CreateIndex("dbo.Attendances", "Attendee_Id");
+            CreateIndex("dbo.Attendances", "CourseID");
+            AddForeignKey("dbo.Attendances", "Attendee_Id", "dbo.AspNetUsers", "Id");
+        }
+    }
+}
